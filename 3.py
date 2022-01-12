ed074511fee3db85c601c7e2c48c7dd10dd30dc1
gaofeifei <gaofeifeigaofei@sina.com>
2022-01-12 12:03:59 +0800

    3.无重复最长子串 滑动窗口

--- 3.py
+++ 3.py
@@ -9,14 +9,19 @@
 #给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 
 def lengthOfLongestSubstring(s):
-
-
-
+    cur,res=[],0
+    for i in range(len(s)):
+        while s[i] in cur:
+            cur.pop(0)#列表滑动窗口，右边进左边出
+        cur.append(s[i])#右侧无论如何都会进入新的
+        res=max(len(cur),res)
+    return res
 
 if __name__ == '__main__':
-    s = "abcabcbb"
-    s = "bbbbb"
+    # s = "abcabcbb"
+    # s = "bbbbb"
     s = "pwwkew"
-    s = ""
-    lengthOfLongestSubstring(s)
+    # s = ""
+    r=lengthOfLongestSubstring(s)
+    print(r)
 
