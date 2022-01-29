0d9fd18c351d05b1308e2f662cd94d624608dede
gaofeifei <gaofeifeigaofei@sina.com>
2022-01-29 11:50:37 +0800

    31

---
+++ 31.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env/python3
+# coding:utf-8
+"""
+# @Time    : 2022/1/29 11:49
+# @Author  : feifeigao
+# @File    : 31.py
+"""
+"""
+31:
+[题意]
+
+[思路题解]
+
+
+[解题步骤]
+
+[经典代码模板]
+
+[tips]
+
+[注意]
+
+
+
+def a(nums):
+    n=len(nums)
+
+if __name__ == '__main__':
+    nums= [1,2,3]
+    a(nums)
+
+"""
